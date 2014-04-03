a33ddbb116ac02d1cf5d79245ca2da11a2475814
TheFrud <frud@live.com>
2014-04-04 00:59:58 +0200

    Test

--- Prototyp_Bibliotek/src/se/prototyp/servlets/LoginServlet.java
+++ Prototyp_Bibliotek/src/se/prototyp/servlets/LoginServlet.java
@@ -20,7 +20,8 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
