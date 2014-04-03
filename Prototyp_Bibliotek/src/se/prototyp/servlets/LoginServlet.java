7a6b08bd8fe9243bc2c787c9714f8e3aa035b5bc
TheFrud <frud@live.com>
2014-04-04 00:57:32 +0200

    Deleted printstatements from LoginServlet.

--- Prototyp_Bibliotek/src/se/prototyp/servlets/LoginServlet.java
+++ Prototyp_Bibliotek/src/se/prototyp/servlets/LoginServlet.java
@@ -20,8 +20,7 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.print("Hej");
-		System.out.println("Hej2");
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
